ba722900cb8978075af5da2b8ede6d81d4563b0b
duytx <duytx@lifetimetech.vn>
2012-11-26 17:00:11 +0700

    Ket noi mang va constant file Signed-off-by: duytx <duytx@lifetimetech.vn>

--- JP_TRAVEL/src/com/jptravel/activity/myalbum/MyAlbumActivity.java
+++ JP_TRAVEL/src/com/jptravel/activity/myalbum/MyAlbumActivity.java
@@ -1,15 +1,41 @@
 package com.jptravel.activity.myalbum;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.jptravel.common.BaseActivity;
+import com.jptravel.common.Constant;
+import com.jptravel.networkmanager.RestClient;
+import com.jptravel.networkmanager.RestClient.RequestMethod;
 
 public class MyAlbumActivity extends BaseActivity {
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		//TODO : Sample connect network and must be remove later
+		RestClient restClient = new RestClient(Constant.BASE_URL +  "places");
+		restClient.addParam("area_id", "1");
+		restClient.addParam("limit", "20");
+		restClient.addParam("offset", "1");
+		
+		RestClient restClientPost = new RestClient(Constant.BASE_URL +  "bookmarks");
+		restClientPost.addParam("bookmarkable_type", "post");
+		restClientPost.addParam("bookmarkable_id", "2");
+		restClientPost.addParam("access_token", "abcdef12345");
+		
+		try {
+			restClient.execute(RequestMethod.GET);
+			Log.d("KET QUA GET", restClient.getResponse());
+			restClientPost.execute(RequestMethod.POST);
+			Log.d("KET QUA POST  ", restClientPost.getResponse());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
