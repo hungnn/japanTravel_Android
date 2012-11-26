e5604193882f6f2d88a9b2cf5e1f3425d5fdcea5
duytx <duytx@lifetimetech.vn>
2012-11-26 11:26:55 +0700

    Event entity Signed-off-by: duytx <duytx@lifetimetech.vn>

---
+++ JP_TRAVEL/src/com/jptravel/entity/Event.java
@@ -0,0 +1,57 @@
+package com.jptravel.entity;
+
+
+public class Event {
+	
+	private String eventId;
+	private String eventPlaceName;
+	private String eventName;
+	private String eventDescription;
+	private String eventStartTime;
+	private String eventEndTime;
+	private String eventImageUrl;
+	
+	public String getEventId() {
+		return eventId;
+	}
+	public void setEventId(String eventId) {
+		this.eventId = eventId;
+	}
+	public String getEventPlaceName() {
+		return eventPlaceName;
+	}
+	public void setEventPlaceName(String eventPlaceName) {
+		this.eventPlaceName = eventPlaceName;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public String getEventDescription() {
+		return eventDescription;
+	}
+	public void setEventDescription(String eventDescription) {
+		this.eventDescription = eventDescription;
+	}
+	public String getEventStartTime() {
+		return eventStartTime;
+	}
+	public void setEventStartTime(String eventStartTime) {
+		this.eventStartTime = eventStartTime;
+	}
+	public String getEventEndTime() {
+		return eventEndTime;
+	}
+	public void setEventEndTime(String eventEndTime) {
+		this.eventEndTime = eventEndTime;
+	}
+	public String getEventImageUrl() {
+		return eventImageUrl;
+	}
+	public void setEventImageUrl(String eventImageUrl) {
+		this.eventImageUrl = eventImageUrl;
+	}
+	
+}
